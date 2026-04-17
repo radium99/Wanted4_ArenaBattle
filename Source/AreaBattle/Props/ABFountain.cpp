2ef511f5c5801be9f7ed53724a59ff4d9ee4a2be
radium99 <115422121+radium99@users.noreply.github.com>
2026-04-17 11:55:00 +0900

    분수대 액터 생성 실습
    - ABFountain 클래스 추가
    - 스태틱 메시 컴포넌트 추가 (2개)
    - ABFountain 생성자에서 2개의 컴포넌트 생성 및 애셋 설정.

---
+++ Source/AreaBattle/Props/ABFountain.cpp
@@ -0,0 +1,60 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Props/ABFountain.h"
+#include "Components/StaticMeshComponent.h"
+
+// Sets default values
+AABFountain::AABFountain()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	Body = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Body"));
+	Water = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Water"));
+
+	// 루트 컴포넌트 (필수)
+	RootComponent = Body;
+
+	// Water는 루트의 하위 컴포넌트로 지정.
+	Water->SetupAttachment(Body);
+
+	// 하위 컴포넌트 설정한 후에는 상대 위치 설정해주기.
+	Water->SetRelativeLocation(FVector(0.0f, 0.0f, 132.0f));
+
+	// 분수대 애셋
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> FountainBodyMesh(
+		TEXT("/Game/ArenaBattle/Environment/Props/SM_Plains_Castle_Fountain_01.SM_Plains_Castle_Fountain_01")
+	);
+
+	if (FountainBodyMesh.Succeeded())
+	{
+		Body->SetStaticMesh(FountainBodyMesh.Object);
+	}
+
+	// 물 애셋
+	static ConstructorHelpers::FObjectFinder<UStaticMesh> FountainWaterMesh(
+		TEXT("/Game/ArenaBattle/Environment/Props/SM_Plains_Fountain_02.SM_Plains_Fountain_02")
+	);
+
+	if (FountainWaterMesh.Succeeded())
+	{
+		Water->SetStaticMesh(FountainWaterMesh.Object);
+	}
+
+}
+
+// Called when the game starts or when spawned
+void AABFountain::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void AABFountain::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
